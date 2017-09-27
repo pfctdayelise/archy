ec003a7bf200bcd562c97012369cbacf32c3ff63
Brianna Laugher <brianna@laugher.id.au>
2017-09-27 22:07:00 +1000

    Add readme, rm config file

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+Based on https://github.com/conbus/fbmq#example
+
+Create a config.py as per https://github.com/conbus/fbmq/blob/master/example/config.py
+
--- config.py
+++
@@ -1,5 +0,0 @@
-CONFIG = {
-    'FACEBOOK_TOKEN': 'EAAEc9rIcxAYBACv6cdsF3ZBQPZCuGwrHMDTKtwZAerGGlnQZBZBhtme8YHkp248Y8kvB3NxfzTqRb5aIhc0t3KwXAdP377WtOBFwu6cLUfaScAlf4KXO5Hp9iFtbxzfGumxkfp8EF9xF4vLakZCVxTRQGyKoJ3oYiTKWEAxhJkmwZDZD',
-    'VERIFY_TOKEN': 'nvStd41FYJNATPUOWgmL',
-    'SERVER_URL': 'https://pfctdayelise.pythonanywhere.com'
-}
